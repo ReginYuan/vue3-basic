bf13adf4a99eb5b2d83c2d950f8f033c1300972b
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-11-18 17:26:42 +0800

    3-8 老瓶新酒 - 生命周期

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <h1>{{ count }}</h1>
+    <h1>{{ double }}</h1>
+    <ul>
+      <li v-for="number in numbers" :key="number">
+        <h1>{{ number }}</h1>
+      </li>
+    </ul>
+    <h1>{{ person.name }}</h1>
+    <button @click="increase">+1</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import {
+  ref,
+  computed,
+  reactive,
+  toRefs,
+  onMounted,
+  onUpdated,
+  onRenderTriggered
+} from "vue";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
+// 定义接口   定义data的类型 DataProps
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+  numbers: number[];
+  person: { name?: string };
+}
+export default {
+  name: "App",
+  setup() {
+    // const count = ref(0);
+    // const double = computed(() => {
+    //   return count.value * 2;
+    // });
+    // const increase = () => {
+    //   count.value++;
+    // };
+
+    onMounted(() => {
+      console.log("mounted");
+    });
+    onUpdated(() => {
+      console.log("updated");
+    });
+    onRenderTriggered(event => {
+      console.log(event);
+    });
+
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => {
+        data.count++;
+      },
+      double: computed(() => data.count * 2),
+      numbers: [0, 1, 2],
+      person: {}
+    });
+
+    data.numbers[0] = 5;
+    data.person.name = "ReginYuan";
+    // toRefs将普通的对象转换成响应式对象
+    const refData = toRefs(data);
+    return {
+      //...展开refData
+      ...refData
+    };
   }
-});
+};
 </script>
 
 <style>
