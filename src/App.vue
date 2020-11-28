aa75b5a15af25509ffe0db16a7689e7f54557142
爱做梦的袁大事 <oncwnuARQZexvg5c0mK5Lmzmt0uY@git.weixin.qq.com>
2020-11-28 21:42:53 +0800

    3-15 Teleport - 瞬间移动 第二部分

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,30 @@
 <template>
   <div id="app">
-    <h1>{{ count }}</h1>
-    <h1>{{ double }}</h1>
-    <h1>{{ greetings }}</h1>
-    <h1 v-if="loading">loading!...</h1>
-    <img v-if="loaded" :src="result[0].url" />
-    <h1>X:{{ x }},Y:{{ y }}</h1>
-    <modal></modal>
-    <button @click="increase">+1</button>
-    <button @click="updateGreetings">Update Title</button>
+    <button @click="openModal">Open Modal</button><br />
+    <modal :isOpen="modalIsOpen" @close-modal="onModalClose"> My Modal</modal>
   </div>
 </template>
 
 <script lang="ts">
-import { ref, computed, reactive, toRefs, watch } from "vue";
-import useMousePosition from "@/hooks/useMousePosition";
-import useURLLoader from "@/hooks/useURLLoader";
-
-// 定义接口   定义data的类型 DataProps
-interface DataProps {
-  count: number;
-  double: number;
-  increase: () => void;
-  numbers: number[];
-  person: { name?: string };
-}
-
-// 在应用中的使用，可以定义不同的数据类型
-interface DogResult {
-  message: string;
-  //xhr.status:获取当前服务器的响应状态  200=>成功
-  status: string;
-}
-
-interface CatResult {
-  id: string;
-  url: string;
-  width: number;
-  height: number;
-}
-
+import { ref } from "vue";
+import Modal from "./components/Modal.vue";
 export default {
   name: "App",
+  components: {
+    Modal
+  },
   setup() {
-    const data: DataProps = reactive({
-      count: 0,
-      increase: () => {
-        data.count++;
-      },
-      double: computed(() => data.count * 2),
-      numbers: [0, 1, 2],
-      person: {}
-    });
-
-    data.numbers[0] = 5;
-    data.person.name = "ReginYuan";
-
-    const { x, y } = useMousePosition();
-    // 使用 urlLoader 展示狗狗图片
-    // const { result, loading, loaded } = useURLLoader<DogResult>(
-    //   "https://dog.ceo/api/breeds/image/random"
-    // );
-
-    // 免费猫图片的 API  https://api.thecatapi.com/v1/images/search?limit=1
-    const { result, loading, loaded } = useURLLoader<CatResult[]>(
-      "https://api.thecatapi.com/v1/images/search?limit=1"
-    );
-    watch(result, () => {
-      if (result.value) {
-        console.log("value", result.value[0].url);
-      }
-    });
-    const greetings = ref("");
-    const updateGreetings = () => {
-      greetings.value += "hello!";
+    const modalIsOpen = ref(false);
+    const openModal = () => {
+      modalIsOpen.value = true;
     };
-
-    watch([greetings, () => data.count], (newValue, oldValue) => {
-      console.log(newValue, oldValue);
-      document.title = "updated" + greetings.value + data.count;
-    });
-    // toRefs将普通的对象转换成响应式对象
-    const refData = toRefs(data);
+    const onModalClose = () => {
+      modalIsOpen.value = false;
+    };
     return {
-      //...展开refData
-      ...refData,
-      greetings,
-      updateGreetings,
-      x,
-      y,
-      result,
-      loading,
-      loaded
+      modalIsOpen,
+      openModal,
+      onModalClose
     };
   }
 };
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,23 +1,38 @@
-<template>
-  <!-- vue3 新添加了一个默认的组件就叫
+ <!-- vue3 新添加了一个默认的组件就叫
   Teleport，我们可以拿过来直接使用，它上面有一个 to 的属性，它接受一个css query
   selector 作为参数，这就是代表要把这个组件渲染到哪个 dom 元素中-->
+  <template>
   <teleport to="#modal">
-    <div id="center">
-      <h2>this is a modal</h2>
+    <div id="center" v-if="isOpen">
+      <h2><slot>this is a modal</slot></h2>
+      <button @click="buttonClick">Close</button>
     </div>
   </teleport>
 </template>
-
 <script lang="ts">
-export default {};
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: {
+    isOpen: Boolean
+  },
+  emits: {
+    "close-modal": null
+  },
+  setup(props, context) {
+    const buttonClick = () => {
+      context.emit("close-modal");
+    };
+    return {
+      buttonClick
+    };
+  }
+});
 </script>
-
 <style>
 #center {
   width: 200px;
   height: 200px;
-  border: 2rpx solid black;
+  border: 2px solid black;
   background: white;
   position: fixed;
   left: 50%;
